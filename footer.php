b0ca322d3504c19a3af5eb52195d0517688ad636
xmartina <martinsprince126@gmail.com>
2024-06-29 03:38:10 +0100

    d

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 <!-- Footer Section Here -->
 <footer class="footer-wrapper section-bg section-padding pb-0">
     <div class="dot-shape">
-        <img src="assets/img/footer/dot-shape.png" alt="dot-img">
+        <img src="/assets/img/footer/dot-shape.png" alt="dot-img">
     </div>
     <div class="arrow-shape-2 wow tpfadeLeft" data-wow-delay=".3s">
-        <img src="assets/img/footer/arrow-shape2.png" alt="arrow-img">
+        <img src="/assets/img/footer/arrow-shape2.png" alt="arrow-img">
     </div>
     <div class="arrow-shape wow tpfadeRight" data-wow-delay=".3s">
-        <img src="assets/img/footer/arrow-shape.png" alt="arrow-img">
+        <img src="/assets/img/footer/arrow-shape.png" alt="arrow-img">
     </div>
     <div class="container">
         <div class="cta-banner-style-1 wow fadeInUp">
-            <div class="cta-banner" data-background="assets/img/cta-banner/banner1.jpg">
+            <div class="cta-banner" data-background="/assets/img/cta-banner/banner1.jpg">
                 <div class="section-title-content mb-0">
                             <span class="text-white">
                             Get Consultations
@@ -32,7 +32,7 @@
                     <div class="single-footer-widget">
                         <div class="widget-head">
                             <a href="/">
-                                <img src="assets/img/logo/footer-logo.png" alt="footer-logo">
+                                <img src="/assets/img/logo/footer-logo.png" alt="footer-logo">
                             </a>
                         </div>
                         <p>
@@ -104,7 +104,7 @@
                         </div>
                         <div class="single-post-items">
                             <div class="post-image">
-                                <img src="assets/img/footer/post1.jpg" alt="post-img">
+                                <img src="/assets/img/footer/post1.jpg" alt="post-img">
                             </div>
                             <div class="post-content">
                                 <ul>
@@ -122,7 +122,7 @@
                         </div>
                         <div class="single-post-items">
                             <div class="post-image">
-                                <img src="assets/img/footer/post2.jpg" alt="post-img">
+                                <img src="/assets/img/footer/post2.jpg" alt="post-img">
                             </div>
                             <div class="post-content">
                                 <ul>
@@ -203,28 +203,28 @@
 </footer>
 
 <!-- Jquery Latest -->
-<script src="assets/js/jquery-3.7.1.min.js"></script>
+<script src="/assets/js/jquery-3.7.1.min.js"></script>
 <!-- Viewport Js -->
-<script src="assets/js/viewport.jquery.js"></script>
+<script src="/assets/js/viewport.jquery.js"></script>
 <!-- Bootstrap Js -->
-<script src="assets/js/bootstrap.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
 <!-- Nice Select Js -->
-<script src="assets/js/jquery.nice-select.min.js"></script>
+<script src="/assets/js/jquery.nice-select.min.js"></script>
 <!-- Waypoints Js -->
-<script src="assets/js/jquery.waypoints.js"></script>
+<script src="/assets/js/jquery.waypoints.js"></script>
 <!-- Counterup Js -->
-<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="/assets/js/jquery.counterup.min.js"></script>
 <!-- Swiper Slider Js -->
-<script src="assets/js/swiper.min.js"></script>
+<script src="/assets/js/swiper.min.js"></script>
 <!-- MeanMenu Js -->
-<script src="assets/js/jquery.meanmenu.min.js"></script>
+<script src="/assets/js/jquery.meanmenu.min.js"></script>
 <!-- Magnific Popup Js -->
-<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="/assets/js/jquery.magnific-popup.min.js"></script>
 <!-- Wow Animation Js -->
-<script src="assets/js/wow.min.js"></script>
+<script src="/assets/js/wow.min.js"></script>
 <!-- Circle Progress Js -->
-<script src="assets/js/circle-progress.js"></script>
+<script src="/assets/js/circle-progress.js"></script>
 <!-- Main.js -->
-<script src="assets/js/main.js"></script>
+<script src="/assets/js/main.js"></script>
 </body>
 </html>
